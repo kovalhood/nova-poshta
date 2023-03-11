0043ff977af9be6cfc4f1427b9ea62bd83da694f
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-03-12 01:33:23 +0200

    Modified open/close schedule status

--- src/components/Branches/BranchesList/BranchesList.jsx
+++ src/components/Branches/BranchesList/BranchesList.jsx
@@ -29,80 +29,116 @@ const BranchesList = ({searchQuery}) => {
         {
             isLoading === true
                 ? <BranchSkeleton />
-                : <>{ branchesData.map(({ SiteKey, CategoryOfWarehouse, WarehouseIndex, ShortAddress, Number, Schedule, TotalMaxWeightAllowed, PlaceMaxWeightAllowed}) => (
-                    <li key={SiteKey} className={s.branches__item}>
-                        <div>
-                            <div className={s.branches__title}>
-                                {CategoryOfWarehouse === "Postomat"
-                                    ? <><h3>Поштомат №{ Number },</h3><span className={s.branches__weight}>&nbsp;до {TotalMaxWeightAllowed > 0 ? TotalMaxWeightAllowed : PlaceMaxWeightAllowed} кг</span></>
-                                    : <><h3>Відділення №{ Number },</h3><span className={s.branches__weight}>&nbsp;до {TotalMaxWeightAllowed > 0 ? TotalMaxWeightAllowed : PlaceMaxWeightAllowed} кг</span></>
-                                }
-                                
+                : <>
+                    {branchesData.map(({ SiteKey, CategoryOfWarehouse, WarehouseIndex, ShortAddress, Number, Schedule, TotalMaxWeightAllowed, PlaceMaxWeightAllowed }) => (
+                        <li key={SiteKey} className={s.branches__item}>
+                            <div>
+                                <div className={s.branches__title}>
+                                    {CategoryOfWarehouse === "Postomat"
+                                        ? <><h3>Поштомат №{ Number },</h3><span className={s.branches__weight}>&nbsp;до {TotalMaxWeightAllowed > 0 ? TotalMaxWeightAllowed : PlaceMaxWeightAllowed} кг</span></>
+                                        : <><h3>Відділення №{ Number },</h3><span className={s.branches__weight}>&nbsp;до {TotalMaxWeightAllowed > 0 ? TotalMaxWeightAllowed : PlaceMaxWeightAllowed} кг</span></>
+                                    }
+                                    
+                                </div>
+                                <p className={s.branches__index}>Індекс { WarehouseIndex }</p>
+                                <p className={s.branches__address}>{ ShortAddress }</p>
                             </div>
-                            <p className={s.branches__index}>Індекс { WarehouseIndex }</p>
-                            <p className={s.branches__address}>{ ShortAddress }</p>
-                        </div>
-
-                        <div className={s.branches__schedule}>
-                            {new Date(currentDate).getDay() === 0 && Schedule.Sunday !== '00:01-23:59'
-                                ? <>{Schedule.Sunday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Sunday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Monday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
-
-                            {new Date(currentDate).getDay() === 1 && Schedule.Monday !== '00:01-23:59'
-                                ? <>{Schedule.Monday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Monday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Tuesday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
-
-                            {new Date(currentDate).getDay() === 2 && Schedule.Tuesday !== '00:01-23:59'
-                                ? <>{Schedule.Tuesday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Tuesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Wednesday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
-
-                            {new Date(currentDate).getDay() === 3 && Schedule.Wednesday !== '00:01-23:59'
-                                ? <>{Schedule.Wednesday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Wednesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Thursday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
-
-                            {new Date(currentDate).getDay() === 4 && Schedule.Thursday !== '00:01-23:59'
-                                ? <>{Schedule.Thursday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Thursday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Friday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
-
-                            {new Date(currentDate).getDay() === 5 && Schedule.Thursday !== '00:01-23:59'
-                                ? <>{Schedule.Friday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Friday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Saturday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
                             
-                            {new Date(currentDate).getDay() === 6 && Schedule.Saturday !== '00:01-23:59'
-                                ? <>{Schedule.Saturday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Saturday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Sunday.slice(0, 5) }</p> }</>
-                                : <></>
-                            }
+                            {/* Schedule dependencies for correct time of open/close display */}
+                            <div className={s.branches__schedule}>
+                                {new Date(currentDate).getDay() === 0 && Schedule.Sunday !== '00:01-23:59' && Schedule.Sunday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Sunday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Sunday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Monday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
 
-                            {Schedule.Sunday === '00:01-23:59' && Schedule.Monday === '00:01-23:59' && Schedule.Tuesday === '00:01-23:59' && Schedule.Wednesday === '00:01-23:59' && Schedule.Thursday === '00:01-23:59' && Schedule.Friday === '00:01-23:59' && Schedule.Saturday === '00:01-23:59'
-                                ? <p className={s.branches__schedule_open}>Відчинено цілодобово</p>
-                                : <></>
-                            }
-                        </div>
+                                {new Date(currentDate).getDay() === 0 && Schedule.Sunday !== '00:01-23:59' && Schedule.Sunday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Sunday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Sunday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Sunday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
 
-                        <a
-                            href={ `http://maps.google.com/maps?q=${ShortAddress.replace(/ /g,"+")}` }
-                            target="_blank"
-                            rel="noreferrer"
-                            className={s.branches__map_link}
-                        >
-                            <svg className={s.branches__map_icon} width="24" height="24">
-                                <use href={`${sprite}#location`}></use>
-                            </svg>
-                        </a>
-                    </li>
-                ))
+                                {new Date(currentDate).getDay() === 1 && Schedule.Monday !== '00:01-23:59' && Schedule.Monday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Monday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Monday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Tuesday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 1 && Schedule.Monday !== '00:01-23:59' && Schedule.Monday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Monday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Monday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Monday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 2 && Schedule.Tuesday !== '00:01-23:59' && Schedule.Tuesday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Tuesday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Tuesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Wednesday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 2 && Schedule.Tuesday !== '00:01-23:59' && Schedule.Tuesday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Tuesday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Tuesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Tuesday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 3 && Schedule.Wednesday !== '00:01-23:59' && Schedule.Wednesday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Wednesday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Wednesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Thursday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 3 && Schedule.Wednesday !== '00:01-23:59' && Schedule.Wednesday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Wednesday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Wednesday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Wednesday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 4 && Schedule.Thursday !== '00:01-23:59' && Schedule.Thursday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Thursday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Thursday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Friday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 4 && Schedule.Thursday !== '00:01-23:59' && Schedule.Thursday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Thursday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Thursday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Thursday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 5 && Schedule.Friday !== '00:01-23:59' && Schedule.Friday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Friday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Friday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Saturday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 5 && Schedule.Friday !== '00:01-23:59' && Schedule.Friday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Friday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Friday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Friday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 6 && Schedule.Saturday !== '00:01-23:59' && Schedule.Saturday.slice(0, 2) <= new Date(currentDate).getHours()
+                                    ? <>{Schedule.Saturday.slice(6, 8) > new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Saturday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;чекаємо завтра з { Schedule.Sunday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {new Date(currentDate).getDay() === 6 && Schedule.Saturday !== '00:01-23:59' && Schedule.Saturday.slice(0, 2) > new Date(currentDate).getHours()
+                                    ? <>{Schedule.Saturday.slice(0, 2) < new Date(currentDate).getHours() ? <p><span className={s.branches__schedule_open}>Відчинено</span>&nbsp;до {Schedule.Saturday.slice(6, 11)}</p> : <p><span className={s.branches__schedule_close}>Зачинено</span>&nbsp;відкриється в { Schedule.Saturday.slice(0, 5) }</p> }</>
+                                    : <></>
+                                }
+
+                                {Schedule.Sunday === '00:01-23:59' && Schedule.Monday === '00:01-23:59' && Schedule.Tuesday === '00:01-23:59' && Schedule.Wednesday === '00:01-23:59' && Schedule.Thursday === '00:01-23:59' && Schedule.Friday === '00:01-23:59' && Schedule.Saturday === '00:01-23:59'
+                                    ? <p className={s.branches__schedule_open}>Відчинено цілодобово</p>
+                                    : <></>
+                                }
+                            </div>
+
+                            {/* Link to Google Maps with address of branch */}
+                            <a
+                                href={ `http://maps.google.com/maps?q=${ShortAddress.replace(/ /g,"+")}` }
+                                target="_blank"
+                                rel="noreferrer"
+                                className={s.branches__map_link}
+                            >
+                                <svg className={s.branches__map_icon} width="24" height="24">
+                                    <use href={`${sprite}#location`}></use>
+                                </svg>
+                            </a>
+                        </li>
+                    ))
                 }
-                </>
-
+            </>
         }
-            
-        </ul>
+    </ul>
 }
 
 export default BranchesList;
--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store.store}>
       <PersistGate loading={null} persistor={store.persistor}>
-        {/* <BrowserRouter basename="/"> */}
-        <BrowserRouter basename="/nova-poshta/">
+        <BrowserRouter basename="/">
+        {/* <BrowserRouter basename="/nova-poshta/"> */}
           <App />
         </BrowserRouter>
       </PersistGate>
