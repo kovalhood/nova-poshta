52cd3768c358197f370d9eedd1c2adac6b800fae
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-03-12 15:36:31 +0200

    Added up button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Wrapper from "./components/Wrapper";
 import TtnPage from "./pages/TtnPage";
 import BranchesPage from "./pages/BranchesPage";
+import ScrollToTop from 'react-scroll-up';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styles from './services/styles';
+import sprite from './images/icons.svg';
 
 function App() {
   return (
@@ -29,6 +32,12 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
 
+      <ScrollToTop showUnder={140} style={styles.upButton}>
+        <svg style={{ fill: '#D9291C' }} width="16" height="16" aria-label="logo">
+            <use href={`${sprite}#arrow-up`}></use>
+        </svg>
+      </ScrollToTop>
+      
       <ToastContainer autoClose={3000} theme="colored" />
     </>
   );
