cb3e7d8a7a8b694d90e63bb8670a0731505c0750
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-03-12 18:05:02 +0200

    Changed branches skeleton width

--- src/components/Branches/BranchesList/BranchSkeleton/BranchSkeleton.jsx
+++ src/components/Branches/BranchesList/BranchSkeleton/BranchSkeleton.jsx
@@ -8,19 +8,19 @@ const BranchSkeleton = ({amountOfCards}) => {
             amountOfCards.map((item) => (
                 <li key={item} className={s.branches__item}>
                     <div className={s.branches__title}>
-                        <Skeleton count={1} width={220} />
+                        <Skeleton count={1} width={200} />
 
                         <div className={s.branches__index}>
-                            <Skeleton count={1} width={170} />
+                            <Skeleton count={1} width={100} />
                         </div>
                         
                         <div className={s.branches__address}>
-                            <Skeleton count={1} width={250} />
+                            <Skeleton count={1} width={240} />
                         </div>
                     </div>
                     
                     <div className={s.branches__schedule}>
-                        <Skeleton count={1} width={200} />
+                        <Skeleton count={1} width={150} />
                     </div>
                 </li>
             ))
