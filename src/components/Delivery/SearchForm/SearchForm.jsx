319a129b1fd5f6ca15b78de86d721771f87555c4
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-03-16 00:44:47 +0200

    Removed unused code

--- src/components/Delivery/SearchForm/SearchForm.jsx
+++ src/components/Delivery/SearchForm/SearchForm.jsx
@@ -42,14 +42,6 @@ const SearchForm = (props) => {
         ["e", "E", "+", "-", ".", ","].includes(event.key) && event.preventDefault();
     }
 
-    // Automatically search last query from LocalStorage after refresh of the page
-    // useEffect(() => {
-    //     if (searches.length!==0) {
-    //         setForm({ ttn: searches[0].number });
-    //         props.onQuerySearch({ ttn: searches[0].number });
-    //     }
-    // }, []);
-
     useEffect(() => {
         if (form.ttn !== props.historyQuery.ttn) {
             setForm(props.historyQuery);
@@ -57,7 +49,7 @@ const SearchForm = (props) => {
     }, [props.historyQuery]);
 
     useEffect(() => {
-        if (searches.length===0) {
+        if (searches.length === 0) {
             setForm({ ttn: '' });
         };
     }, [searches]);
